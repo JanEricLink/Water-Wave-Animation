fba688a5783403f4085f23515b54ca5ca0f95d65
JanEricLink <145751242+JanEricLink@users.noreply.github.com>
2024-07-26 10:36:56 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,39 @@
+let points;
+let b;
+let size;
+
 function setup() {
-  createCanvas(400, 400,WEBGL);
+  angleMode(DEGREES);
+  createCanvas(800, 800, WEBGL);
+  points = [];
+  b = 0;
+  size = 10;
+  camera(0, size * -10 - 25, size * 20 + 50);
 }
 
 function draw() {
   background(220);
   orbitControl();
-  sphere(30);
+  strokeWeight(0.2);
+
+  for (let a = 0; a < size * 36; a++) {
+    points[a] = sin(10 * (a - b));
+  }
+
+  push();
+  noStroke();
+  fill(200, 0, 0);
+  translate(0, 2 * points[15], 0);
+  sphere(2);
+  pop();
+
+  for (var key in points) {
+    push();
+    rotateX(90);
+
+    translate(0, 0, 2 * points[key]);
+    torus(0.5 * key + 1.25, 1, 24, 1);
+    pop();
+  }
+  b++;
 }
